30516aa224dff129deb9548e377b579cb4d12ef8
andreagonz <andreagonz@ciencias.unam.mx>
2016-03-08 22:22:01 -0600

    Archivo SQL
    Archivo sql con tablas para poblar la base

---
+++ Practica1/practica1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE usuario(
+       id INTEGER PRIMARY KEY,
+       nombre VARCHAR(64) NOT NULL,
+       correo VARCHAR(128) NOT NULL,
+       contrasenia VARCHAR(128) NOT NULL,
+       idpais INTEGER NOT NULL
+ );
+
+CREATE TABLE pais(
+       id INTEGER PRIMARY KEY,
+       nombre VARCHAR(64)
+);
