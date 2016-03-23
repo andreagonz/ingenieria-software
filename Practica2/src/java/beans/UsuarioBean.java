cc50e2d30a520fa5fce3b62cf5c7135ab7fb1d27
andreagonz <andreagonz@ciencias.unam.mx>
2016-03-22 20:17:30 -0600

    Beans
    Bean para clase usuario

---
+++ Practica2/src/java/beans/UsuarioBean.java
@@ -0,0 +1,71 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import dao.UsuarioDAO;
+import model.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class UsuarioBean {
+
+	private Usuario usuario = new Usuario();
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String verificarDatos() throws Exception {
+		UsuarioDAO usuDAO = new UsuarioDAO();
+		Usuario us;
+		String resultado;
+
+		try {
+			// Enviando la encriptacion
+			//String encript = DigestUtils.md5Hex(this.usuario.getNombre());
+			String encript = DigestUtils.sha1Hex(this.usuario.getContrasenia());
+			this.usuario.setContrasenia(encript);
+
+			us = usuDAO.verificarDatos(this.usuario);
+			if (us != null) {
+
+				FacesContext.getCurrentInstance().getExternalContext()
+						.getSessionMap().put("usuario", us);
+				resultado = "exito";
+			} else {
+				resultado = "error";
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+
+		return resultado;
+	}
+
+	public boolean verificarSesion() {
+		boolean estado;
+
+		if (FacesContext.getCurrentInstance().getExternalContext()
+				.getSessionMap().get("usuario") == null) {
+			estado = false;
+		} else {
+			estado = true;
+		}
+
+		return estado;
+	}
+
+	public String cerrarSesion() {
+		FacesContext.getCurrentInstance().getExternalContext()
+				.invalidateSession();
+		return "index";
+	}
+}
